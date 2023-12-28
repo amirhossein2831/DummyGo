c66590b1046c4d50ddd50541ad348f28ce660179
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2023-12-29 00:37:52 +0330

    :hammer: chore: instantiate the router and server http server:

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/amirhossein2831/DummyGo
 go 1.21
 
 require (
-	github.com/go-chi/chi/v5 v5.0.11 // indirect
-	github.com/go-chi/cors v1.2.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/go-chi/chi/v5 v5.0.11
+	github.com/joho/godotenv v1.5.1
 )
--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"github.com/amirhossein2831/DummyGo/Error"
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 	err := godotenv.Load()
 	Error.CheckError(err)
 
+	router := chi.NewRouter()
+
+	log.Printf("The app start in port %v", os.Getenv("APP_PORT"))
+	err = http.ListenAndServe(":"+os.Getenv("APP_PORT"), router)
+	Error.CheckError(err)
 }
