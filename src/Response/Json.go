c54eb2bf7aa903bdb340c2e98b30402927e249c6
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2023-12-29 01:46:32 +0330

    :heavy_plus_sign: feat: create ToJson file

--- src/Response/Json.go
+++ src/Response/Json.go
@@ -16,20 +16,3 @@ func ToJson(w http.ResponseWriter, statusCode int, data interface{}) {
 	err := json.NewEncoder(w).Encode(data)
 	JsonError(w, err)
 }
-
-func ToStruct(w http.ResponseWriter, r *http.Request, data interface{}) {
-	decoder := json.NewDecoder(r.Body)
-	decoder.DisallowUnknownFields()
-	err := decoder.Decode(&data)
-	JsonError(w, err)
-}
-
-func JsonError(w http.ResponseWriter, err error) {
-	if err != nil {
-		payload := ErrorResponse{
-			Error:   true,
-			Message: err.Error(),
-		}
-		ToJson(w, http.StatusBadRequest, payload)
-	}
-}
