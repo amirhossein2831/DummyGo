f5efbd5a1853f82166a8ca09723ddb6f26c5f0e2
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2023-12-31 00:05:23 +0330

    :hammer: chore: add api/v1 prifix to all route

--- src/Router/router.go
+++ src/Router/router.go
@@ -2,12 +2,20 @@ package Router
 
 import (
 	"github.com/amirhossein2831/DummyGo/src/Middleware"
+	Router "github.com/amirhossein2831/DummyGo/src/Router/Routes"
 	"github.com/go-chi/chi/v5"
 )
 
 func Route() *chi.Mux {
 	router := chi.NewRouter()
+
+	//Add Middleware Here
 	Middleware.CorsMiddleware(router)
 
+	//Api Routes
+	router.Route("/api/v1", func(r chi.Router) {
+		//Add Routes Here
+		Router.HomeRoutes(r)
+	})
 	return router
 }
